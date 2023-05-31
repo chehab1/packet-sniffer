e33a8acc3a422ba4f1da82dfe40c7ccd0b37cbb4
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2023-06-01 01:13:55 +0300

    won't change

--- venv/PacketSniffer/SnifferServer/Consumers.py
+++ venv/PacketSniffer/SnifferServer/Consumers.py
@@ -10,34 +10,31 @@ import time
 import threading
 import random
 
+
 def fn():
-   f = open(r"D:\00-GitHub\Packet-Sniffer\packet-sniffer\venv\PacketSniffer\SnifferServer\data.json")
-   data = json.load(f)
-   f.close()
-   return data
+    f = open(
+        r"D:\00-GitHub\Packet-Sniffer\packet-sniffer\venv\PacketSniffer\SnifferServer\data.json")
+    data = json.load(f)
+    f.close()
+    return data
 
 
 class SnifferConsumer(WebsocketConsumer):
 
-   def connect(self):
-      self.connection = True
-      self.room_group_name = 'test'
+    def connect(self):
+        self.connection = True
+        self.room_group_name = 'test'
 
-      self.accept()
+        self.accept()
 
+        print("connection started")
+        print("#######################################")
+        print(self.connection)
 
-      print("connection started")
-      print("#######################################")
-      print(self.connection)
-
-
-
-      
-   def disconnect(self, code):
-      self.stop = True
-      del self.thread
-      print("connection closed")
-      print("#######################################")
-      print(self.connection)
-      StopConsumer()
-        
+    def disconnect(self, code):
+        self.stop = True
+        del self.thread
+        print("connection closed")
+        print("#######################################")
+        print(self.connection)
+        StopConsumer()
