797eff8f008d80f9d9e963583e87c181126a692c
MoustafaAshraf8 <mostafa.ashraf8642@gmail.com>
2023-05-31 23:57:17 +0300

    asgi routing + channels setup

--- venv/PacketSniffer/PacketSniffer/settings.py
+++ venv/PacketSniffer/PacketSniffer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'SnifferServer'
 ]
+
+ASGI_APPLICATION = 'PacketSniffer.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
